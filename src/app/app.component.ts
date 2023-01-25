55a91f8063a4cc3df726b726d6cca839fbe38fd7
jimmy <jimmyhd_19@hotmail.com>
2023-01-24 20:58:12 -0500

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-data-encryption-angular';
+	constructor(private _formBuilder: FormBuilder) {}
+
+	formGroup = this._formBuilder.nonNullable.group({
+		names: [''],
+		claim: ['']
+	});
+
+	formDataInStorage?: IForm;
+
+	clickSend(): void {}
+}
+
+interface IForm {
+	names: '';
+	claim: '';
 }
